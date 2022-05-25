9a260a673f67b0a385a51b8914d2f4a0db1de384
Bao Truong LG <bao.truong@lge.com>
2022-05-25 16:47:02 +0700

    handle UI header

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+
+const image = {
+    logo: require('~/assets/img/logo.svg').default,
+    xMark: require('~/assets/img/circle-xmark.svg').default,
+    searchHeader: require('~/assets/img/search-header.svg').default
+}
+
+export default image
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind'
+import images from '~/assets'
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return <header className={cx('wrapper')}>
-        <div className={cx('content')}></div>
+        <div className={cx('content')}>
+            <img src={images.logo} alt="Tiktok logo" />
+            <div className={cx('search')}>
+                <input placeholder="Search accounts and videos" spellCheck={false} />
+                <button className={cx('clear')}>
+                    <img src={images.xMark} alt="xmark" />
+                </button>
+                {/* <FontAwesomeIcon className={cx('loading')} icon={faCircleNotch} /> */}
+                <button className={cx('search-btn')}>
+                    <img src={images.searchHeader} alt="search header" />
+                </button>
+            </div>
+            <div className={cx('actions')}></div>
+        </div>
     </header>
 }
 
